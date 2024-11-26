e6c6c787a9e67c3cc6770666c2bfd95122965a6c
zmwv823 <913720128@qq.com>
2024-11-26 23:01:44 +0800

    Try fix node import problem.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
-# only import if running as a custom node
-try:
-	pass
-except ImportError:
-	pass
-else:
-	NODE_CLASS_MAPPINGS = {}
+# # only import if running as a custom node
+# try:
+# 	pass
+# except ImportError:
+# 	pass
+# else:
+# 	NODE_CLASS_MAPPINGS = {}
 
-# Image Generation
-  ## Sana
-	from .Image_Generation.Sana.nodes import NODE_CLASS_MAPPINGS as Sana_Nodes
-	NODE_CLASS_MAPPINGS.update(Sana_Nodes)
+# # Image Generation
+#   ## Sana
+# 	from .Image_Generation.Sana.nodes import NODE_CLASS_MAPPINGS as Sana_Nodes
+# 	NODE_CLASS_MAPPINGS.update(Sana_Nodes)
  
-NODE_DISPLAY_NAME_MAPPINGS = {k:v.TITLE for k,v in NODE_CLASS_MAPPINGS.items()}
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+# NODE_DISPLAY_NAME_MAPPINGS = {k:v.TITLE for k,v in NODE_CLASS_MAPPINGS.items()}
+# __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+from .Image_Generation.Sana.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
